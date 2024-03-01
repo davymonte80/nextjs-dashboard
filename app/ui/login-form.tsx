2ed90b5ed7ce0679057ae1692b7f31169318a70c
davymonte80 <davymonte80@gmail.com>
2024-03-01 06:42:08 +0300

    update

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -10,9 +10,7 @@ export default function LoginForm() {
   return (
     <form className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={`    mb-3 text-2xl`}>
-          Please log in to continue.
-        </h1>
+        <h1 className={`    mb-3 text-2xl`}>Please log in to continue.</h1>
         <div className="w-full">
           <div>
             <label
@@ -38,6 +36,7 @@ export default function LoginForm() {
               className="mb-3 mt-5 block text-xs font-medium text-gray-900"
               htmlFor="password"
             >
+              
               Password
             </label>
             <div className="relative">
