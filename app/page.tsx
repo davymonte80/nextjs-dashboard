d02f1acab350d92bcfef0277e6c30e2d90ea3119
davymonte80 <davymonte80@gmail.com>
2024-03-14 01:08:29 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import Invoice from './dashboard/Invoice/page';
 
 export default async function Page() {
   return (
