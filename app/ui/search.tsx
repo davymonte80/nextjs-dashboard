0cc1458ec65e0825d46f3c444859f6bf9621a23c
davymonte80 <davymonte80@gmail.com>
2024-03-15 05:59:42 +0300

    update

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,56 +1,22 @@
 'use client';
-
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, useRouter, usePathname } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
-  const pathname = usePathname ();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
-const { replace } = useRouter();
-  const searchTerm = searchParams.get('searchTerm');
-
-  console.log(searchTerm);
-  
-
-  // const [searchTerm, setSearchTerm] = useState(searchParams.get('searchTerm') || '');
-
-  // useEffect(() => {
-  //   searchParams.set('searchTerm', searchTerm);
-  // }, [searchTerm]);
-
-  // console.log(searchTerm);
-
- function handleSearch(term: string) {
-  const params = new URLSearchParams(searchParams);
-  if (term) {
-    params.set('searchTerm', term);
-  } else {params.delete('query');}
-  replace(`${pathname}?${params.toString()}`);
-  //   setSearchTerm(term);
-  // }
-
-  // console.log(searchTerm);
-
-  // console.log(searchParams.get('searchTerm'));
-
-  console.log(searchParams.get('searchTerm'));
-
-  console.log(searchParams.get('searchTerm'));
-
-  console.log(searchParams.get('searchTerm'));
-
-  console.log(searchParams.get('searchTerm'));
-
-  console.log(searchParams.get('searchTerm'));
-
-  console.log(searchParams.get('searchTerm'));
-
-  console.log(searchParams.get('searchTerm'));
-
-  console.log(searchParams.
   function handleSearch(term: string) {
-    console.log(term);
+    console.log(`Searching... ${term}`);
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('searchTerm', term);
+    } else {
+      params.delete('searchTerm');
+    }
+
+    replace(`${pathname}?searchTerm=${params.toString()}`); // Include the parameter name "searchTerm"
   }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -61,6 +27,7 @@ const { replace } = useRouter();
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
